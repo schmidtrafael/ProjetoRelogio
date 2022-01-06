80a1ed492a7538e539143e1aa7c0454952f4bce0
Rafael <48494280+schmidtrafael@users.noreply.github.com>
2022-01-05 22:20:45 -0300

    update
    Segundos corrigidos, agora com uma transição suave do 59 para o 1 segundo

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 const PONTEIROHORA = document.querySelector("#hour");
 const PONTEIROMINUTO = document.querySelector("#minute");
 const PONTEIROSEGUNDO = document.querySelector("#second");
-
-let posicaoHr = 20;
-let posicaoMin = 130;
-let posicaoSeg = 267;
-
+/* calcula a hora do computador*/
 var data = new Date();
 console.log(data);
 let hr = data.getHours();
@@ -13,12 +9,21 @@ let min = data.getMinutes();
 let seg = data.getSeconds();
 
 console.log("hora: " + hr + " Minuto: " + min + " Segundo: " + seg);
-
+/*calcula a hora, segundos, minutos para saber a posiçao dos ponteiros*/
 let posicaoHr = (hr*360/12)+(min*(360/60)/12);
 let posicaoMin = (min*360/60)+(seg*(360/60)/60);
 let posicaoSeg = seg*360/60;
 
-PONTEIROHORA.style.transform = "rotate(" + posicaoHr + "deg)";
-PONTEIROMINUTO.style.transform = "rotate(" + posicaoMin + "deg)";
-PONTEIROSEGUNDO.style.transform = "rotate(" + posicaoSeg + "deg)";
+function executarRelogio() {
+ /* corrige bug do ponteiro de um segundo */
+    posicaoHr = posicaoHr+(3/360);
+    posicaoMin = posicaoMin+(6/60);
+    posicaoSeg = posicaoSeg+6;
 
+    PONTEIROHORA.style.transform = "rotate(" + posicaoHr + "deg)";
+    PONTEIROMINUTO.style.transform = "rotate(" + posicaoMin + "deg)";
+    PONTEIROSEGUNDO.style.transform = "rotate(" + posicaoSeg + "deg)";
+
+}
+
+var intervalo = setInterval(executarRelogio, 1000);
